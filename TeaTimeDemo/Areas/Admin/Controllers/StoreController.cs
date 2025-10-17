afce54e0466ac57fda9a26e0c4f2c5c832e4bf71
AnliHsieh <anlihsieh@gmail.com>
2025-10-17 20:47:21 +0800

    first commit

---
+++ TeaTimeDemo/Areas/Admin/Controllers/StoreController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore.Metadata;
+using TeaTimeDemo.DataAccess.Data;
+using TeaTimeDemo.DataAccess.Repository;
+using TeaTimeDemo.DataAccess.Repository.IRepository;
+using TeaTimeDemo.Models;
+using TeaTimeDemo.Models.ViewModels;
+using TeaTimeDemo.Utility;
+
+namespace TeaTimeDemo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+    public class StoreController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public StoreController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            List<Store> objStoreList = _unitOfWork.Store.GetAll().ToList();
+            return View(objStoreList);
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return View(new Store());
+            }
+            else
+            {
+                Store storeObj = _unitOfWork.Store.Get(u => u.Id == id);
+                return View(storeObj);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Upsert(Store storeObj)
+        {
+            if (ModelState.IsValid)
+            {
+                if (storeObj.Id == 0)
+                {
+                    _unitOfWork.Store.Add(storeObj);
+                }
+                else
+                {
+                    _unitOfWork.Store.Update(storeObj);
+                }
+
+                _unitOfWork.Save();
+
+                TempData["success"] = "店鋪新增成功！";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(storeObj);
+            }
+        }
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<Store> ObjStoreList = _unitOfWork.Store.GetAll().ToList();    
+            return Json(new { data= ObjStoreList }); 
+        }
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+
+            var storeToBeDelete = _unitOfWork.Store.Get(u => u.Id == id);
+            if (storeToBeDelete == null)
+            {
+                return Json(new { success = false, message = "刪除失敗" });
+            }
+            else
+            {
+                _unitOfWork.Store.Remove(storeToBeDelete);
+                _unitOfWork.Save();
+                return Json(new { success = true, message = "刪除成功" });
+            }
+        }
+        #endregion
+    }
+}
+
---
+++ TeaTimeDemo/Program.cs
@@ -0,0 +1,56 @@
+using Microsoft.EntityFrameworkCore;
+using TeaTimeDemo.DataAccess.Data;
+using TeaTimeDemo.DataAccess.Repository;
+using TeaTimeDemo.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using TeaTimeDemo.Utility;
+
+namespace TeaTimeDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            builder.Services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+            builder.Services.ConfigureApplicationCookie(options => 
+            {
+                options.LoginPath = $"/Identity/Account/Login";
+                options.LogoutPath = $"/Identity/Account/Logout";
+                options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
+            });
+            builder.Services.AddRazorPages();
+            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+            builder.Services.AddScoped<IEmailSender, EmailSender>();
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            
+            app.UseAuthentication(); //身分驗證
+            app.UseAuthorization(); //授權
+            app.MapRazorPages();
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{area=Custmor}/{controller=Home}/{action=Index}/{id?}");
+            app.Run();
+        }
+    }
+}
